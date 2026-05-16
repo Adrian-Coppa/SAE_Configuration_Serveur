461975b7549f7bc1176b6a3410f844a81bebb95d
Adrian-Coppa <adrian.coppa@outlook.fr>
2026-05-16 22:53:48 +0200

    Fix: Use -L test for symlink verification instead of -f in step2-verify.sh

--- scenarios/step2-verify.sh
+++ scenarios/step2-verify.sh
@@ -1,6 +1,4 @@
-### `step2-verify.sh`
-```bash
 #!/bin/bash
 # Vérifie si le fichier de conf existe et si le lien symbolique d'activation est présent
-[ -f /etc/apache2/sites-available/mon-site.conf ] && [ -f /etc/apache2/sites-enabled/mon-site.conf ] && exit 0
+[ -f /etc/apache2/sites-available/mon-site.conf ] && [ -L /etc/apache2/sites-enabled/mon-site.conf ] && exit 0
 exit 1
