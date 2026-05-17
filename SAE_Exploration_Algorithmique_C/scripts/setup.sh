b3f2b5e267430612a094b386f4471d3a2b3cf539
Adrian-Coppa <adrian.coppa@outlook.fr>
2026-05-17 15:11:17 +0200

    Fix setup.sh: use relative paths for assets

--- SAE_Exploration_Algorithmique_C/scripts/setup.sh
+++ SAE_Exploration_Algorithmique_C/scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /root/annuaire
-cp /root/assets/fonctions.h /root/annuaire/
-cp /root/assets/fonctions.c /root/annuaire/
-cp /root/assets/main.c /root/annuaire/
-cp /root/assets/annuaire_100_entrees.csv /root/annuaire/
+cp ./assets/fonctions.h /root/annuaire/
+cp ./assets/fonctions.c /root/annuaire/
+cp ./assets/main.c /root/annuaire/
+cp ./assets/annuaire_100_entrees.csv /root/annuaire/
 echo "Fichiers sources copiés dans /root/annuaire/"
